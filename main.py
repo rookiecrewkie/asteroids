39b55b71abae84024e6d249f0bbb6a19b48d81eb
Artyom Kreynes <kreynes@gmail.com>
2026-01-30 14:52:11 +0300

    Added CircleShape & Player classes to render Player

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(640.0, 360.0)
 
     while True:
         log_state()
@@ -12,7 +16,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
+        
+        
         
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
